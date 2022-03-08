325ce8a75e8e7fff024bc9b3aa6ef1ec43c3dd97
saranovaty <sara.novati@runtastic.com>
2022-03-08 12:02:30 +0100

    added GitHub url

--- src/App.js
+++ src/App.js
@@ -3,5 +3,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is created by Sara Novati and is{" "}
+        <a
+          href="https://github.com/saranovaty/react-weather-app"
+          target="_blank"
+        >
+          open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
