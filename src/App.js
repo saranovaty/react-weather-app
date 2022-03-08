8b6850d0ad0ac85ad7a300f0aee910215a713d98
saranovaty <sara.novati@runtastic.com>
2022-03-08 12:45:10 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by Sara Novati and is{" "}
-        <a
-          href="https://github.com/saranovaty/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is created by Sara Novati and is{" "}
+          <a
+            href="https://github.com/saranovaty/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
