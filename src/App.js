04ca5044093d08102e2f67bb9ac76f1dc57d0e74
saranovaty <sara.novati@runtastic.com>
2022-03-10 19:14:03 +0100

    added city

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Lisbon" />
         <footer>
           This project is created by Sara Novati and is{" "}
           <a
